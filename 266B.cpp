98c82882ba3b3420d0710acb5921a9a2f343faa3
SafinBhuiyan <112971550+SafinBhuiyan@users.noreply.github.com>
2022-10-31 00:40:03 +0600

    Add files via upload

---
+++ 266B.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,t;
+    int count_swap=0;
+    cin>>n>>t;
+    string str;
+    cin>>str;
+ 
+    while(t--){
+    for(int i=0;i<n;i++)
+    {
+        if(str[i]=='B'&&str[i+1]=='G')
+        
+        {
+            
+            
+            swap(str[i],str[i+1]);
+            i++;
+            
+        
+ 
+        }
+    }
+ 
+    }
+ 
+    cout<<str;
+ 
+}
