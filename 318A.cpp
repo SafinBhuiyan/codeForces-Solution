0c19cf21335d5c01417b3876d2dfd54db93cc0a7
Safin Bhuiyan <112971550+SafinBhuiyan@users.noreply.github.com>
2022-10-31 11:41:34 +0600

    Add files via upload

---
+++ 318A.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    long long int a, b, c;
+    cin >> a >> b;
+    if(a%2 == 0)
+    {
+        c = a / 2;
+    }
+    else
+    {
+        c = (a + 1) / 2;
+    }
+    if(b <= c)
+    {
+        cout << (b * 2) - 1;
+    }
+    else
+    {
+        cout << (b - c) * 2;
+    }
+}
