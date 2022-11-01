151200c1b3c2a71aed9fecf5fc4968966c404472
Safin Bhuiyan <112971550+SafinBhuiyan@users.noreply.github.com>
2022-11-01 23:38:21 +0600

    Update 231A.cpp

--- 231A.cpp
+++ 231A.cpp
@@ -1,13 +1,7 @@
-
 #include<iostream>
 using namespace std;
 int main()
 {
-	#ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin); 
-        freopen("output.txt", "w", stdout);
-	#endif
-
     int n,a,b,c,count=0;
     cin>>n;
     for(int i=0; i<n; i++)
